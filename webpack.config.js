203efefb771bd935ec310f23e5795b2182739809
Rúben França <ruben_tiagof@hotmail.com>
2019-05-18 21:28:11 +0100

    Add minifiers

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const devMode = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: devMode ? 'development' : 'production',
     entry: './src/Main.js',
     output: {
         filename: 'Main.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
