8e323b4c324e8daffd5006d6263445d86ba89851
Rúben França <ruben_tiagof@hotmail.com>
2019-05-18 19:38:43 +0100

    Add css rules to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         filename: 'Main.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // Adds css to the DOM injecting the <style> tag
+                'css-loader', // interprets @import, url() ...
+            ]
+        }]
     }
 };
